e319d6b9b8411ac012f2f09e26c6c52311ffb250
Etienne Millon <etienne.millon@gmail.com>
2011-07-05 18:33:13 +0200

    Split punbb-specific stuff

---
+++ bb2mdir/punbb.py
@@ -0,0 +1,31 @@
+from mailbox import Maildir, MaildirMessage
+from datetime import datetime
+from html2text import html2text
+
+def find_date(soup):
+    "Return the date of a message as a datetime object"
+    date = soup.find('a').text
+
+    if date .startswith('Today'):
+        today = datetime.today().strftime('%Y-%m-%d')
+        date = today + date[5:]
+
+    date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
+    return date
+
+def parse_div(soup):
+    "Parses a message div (class blockpost) and return a MaildirMessage"
+    print soup['id']
+    msg = MaildirMessage()
+
+    msg['Date'] = find_date(soup).strftime("%a, %d %b %Y %H:%M:%S %z")
+    msg['From'] = '%s <nobody@localhost>' % soup.find('strong').text
+    msg['Subject'] = soup.find('h3').text
+
+    body = soup.find('div', 'postmsg')
+    body = html2text(str(body).decode('utf-8'))
+
+    msg.set_payload(body, 'UTF-8')
+
+    return msg
+
--- p2m.py
+++ p2m.py
@@ -12,35 +12,7 @@ think this stuff is worth it, you can buy me a beer in return.
 
 from BeautifulSoup import BeautifulSoup
 from mailbox import Maildir, MaildirMessage
-from datetime import datetime
-from html2text import html2text
-
-def find_date(soup):
-    "Return the date of a message as a datetime object"
-    date = soup.find('a').text
-
-    if date .startswith('Today'):
-        today = datetime.today().strftime('%Y-%m-%d')
-        date = today + date[5:]
-
-    date = datetime.strptime(date, '%Y-%m-%d %H:%M:%S')
-    return date
-
-def parse_div(soup):
-    "Parses a message div (class blockpost) and return a MaildirMessage"
-    print soup['id']
-    msg = MaildirMessage()
-
-    msg['Date'] = find_date(soup).strftime("%a, %d %b %Y %H:%M:%S %z")
-    msg['From'] = '%s <nobody@localhost>' % soup.find('strong').text
-    msg['Subject'] = soup.find('h3').text
-
-    body = soup.find('div', 'postmsg')
-    body = html2text(str(body).decode('utf-8'))
-
-    msg.set_payload(body, 'UTF-8')
-
-    return msg
+from bb2mdir import punbb
 
 def main():
     "Program entry point"
@@ -49,7 +21,7 @@ def main():
     maildir = Maildir('out')
 
     for msg_soup in soup.findAll('div', 'blockpost'):
-        msg = parse_div(msg_soup)
+        msg = punbb.parse_div(msg_soup)
         maildir.add(msg)
 
 if __name__ == "__main__":
