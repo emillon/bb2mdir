b7489e0a75e0af94d1c40266db69430ca0374c12
Etienne Millon <etienne.millon@gmail.com>
2011-07-05 17:39:39 +0200

    Author, maildir out

--- p2m.py
+++ p2m.py
@@ -1,20 +1,35 @@
 from BeautifulSoup import BeautifulSoup
-from mailbox import MaildirMessage
+from mailbox import Maildir, MaildirMessage
 from datetime import datetime
+import time
 
 doc = open('ex.html').read()
 
 soup = BeautifulSoup(doc)
 
-for s in soup.findAll('div', 'blockpost'):
-    #m = MaildirMessage()
-
-    # Date
+def findDate(s):
     dateStr = s.find('a').text
 
     if dateStr.startswith('Today'):
         today = datetime.today().strftime('%Y-%m-%d')
         dateStr = today + dateStr[5:]
-    
+
     date = datetime.strptime(dateStr, '%Y-%m-%d %H:%M:%S')
-    print date
+    return date
+
+def findAuthor(s):
+    return s.find('strong').text
+
+md=Maildir('out')
+
+for s in soup.findAll('div', 'blockpost'):
+    m = MaildirMessage()
+
+    m['Date'] = findDate(s).strftime("%a, %d %b %Y %H:%M:%S %z")
+    m['From'] = '%s <nobody@localhost>' % findAuthor(s)
+
+    m.set_payload(s.find('div', 'postmsg').text, 'UTF-8')
+
+    md.add(m)
+
+    
